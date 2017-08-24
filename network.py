9791f679dce6053878b10c52c2fb674484363039
Dale Smith <dtsmith@mindspring.com>
2017-08-24 14:27:18 -0400

    remove images; further work on backprop; Keras experiments

--- network.py
+++ network.py
@@ -1,6 +1,13 @@
+"""
+http://neuralnetworksanddeeplearning.com/chap1.html
+
+ported to Python 3.6.
+"""
+
 import numpy as np
 
 from activation_functions import sigmoid
+from activation_functions import sigmoid_prime
 
 
 class Network():
@@ -10,10 +17,12 @@ class Network():
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
+
     def feedforward(self, a):
         for bias, weight in zip(self.biases, self.weights):
             a = sigmoid(np.dot(weight, a) + bias)
         return a
+
 
     def SGD(self, training_data, epochs: int, mini_batch_size: int, eta: float, test_data=None):
         if test_data:
@@ -40,3 +49,49 @@ class Network():
             nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
         self.weights = [w - eta_over_n * nw for w, nw in zip(self.weights, nabla_w)]
         self.baises = [b - eta_over_n * nb for b, nb in zip(self.biases, nabla_b)]
+
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x]  # list to store all the activations, layer by layer
+        zs = []  # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * \
+                sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 of the book.  Here,
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations-y)
+
+if __name__ == "__main__":
+    import mnist_loader
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    net = Network([784, 30, 10])
+    net.SGD(list(training_data), 30, 10, 3.0, test_data=list(test_data))
